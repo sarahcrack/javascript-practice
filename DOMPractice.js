284b4ce2b0512c13e8fcc6c0c9405d376164aca0
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2022-10-18 20:11:10 +0100

    initial commit
    JS practice upload

---
+++ DOMPractice.js
@@ -0,0 +1,25 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Your code here...
+//   var header1 = document.getElementsByTagName("h1");
+//   console.log(header1);
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var newParagraph = document.createElement("p");
+  newParagraph.innerText = "Hello this is a JS paragraph";
+  newParagraph.style.fontFamily = "Arial";
+  newParagraph.style.fontSize = "50px";
+  newParagraph.style.border = "10px, black";
+
+  newParagraph.addEventListener("mouseover", function (event) {
+    event.target.style.color = "red";
+    event.target.innerText = "Hello!";
+  });
+
+  newParagraph.addEventListener("mouseleave", function (event) {
+    event.target.style.color = "black";
+    event.target.innerText = "Hello this is a JS paragraph!";
+  });
+
+  document.body.appendChild(newParagraph);
+});
---
+++ app.js
@@ -0,0 +1,80 @@
+// for (i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+
+// function sum(number1, number2) {
+//   return number1 + number2;
+// }
+
+// console.log(sum(2, 3));
+
+// function division(number1, number2) {
+//   return number1 / number2;
+// }
+
+// console.log(division(10, 2));
+
+// const favouriteNumber = sum(2, 9);
+
+// function myName() {
+//   let name = prompt("What is your name?");
+//   return name;
+// }
+
+// console.log(myName());
+
+// function movieDetails(title, released) {
+//   return `${title} came out in ${released}`;
+// }
+
+// let movie1 = movieDetails("Mean Girls", 2004);
+
+// console.log(movie1);
+
+// function shoppingCart(item, cost, balance) {
+//   if (balance >= cost) {
+//     return `Yay you can buy the ${item}`;
+//   } else {
+//     return `Sorry, you don't have enough for ${item} with a balance of ${balance} as it costs ${cost} but please look out for our future deals!`;
+//   }
+// }
+
+// console.log(shoppingCart("dress", 20, 30));
+
+// function reverseString(string) {
+//   return string.reverse();
+// }
+
+function sum(number1, number2) {
+  return number1 + number2;
+}
+
+// console.log(sum(5, 2));
+
+// function power(number1, number2) {
+//   return number1 ** number2;
+// }
+
+// console.log(power(2, 3));
+
+// function powerPrompt() {
+//   let number1 = prompt("What is your first number?");
+//   let number2 = prompt("What is your second number?");
+//   return power(number1, number2);
+// }
+
+// var yourPower = powerPrompt();
+
+// console.log(yourPower);
+
+var travelObject = {
+  favouriteCity: "Bangkok",
+  favouriteFood: "Italian",
+  favouriteAirline: "Emirates",
+  sayHello: function helloWorld() {
+    console.log(sayHello());
+  },
+};
+
+console.log(travelObject.favouriteAirline);
+sayHello();
