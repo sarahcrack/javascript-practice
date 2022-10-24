d062d613fd4b62d79707d015cc0a45d4fa5cc0b4
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2022-10-24 08:37:44 +0100

    playing around with quiz JS syntax to create alert and link to another html page

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ function sayHello(event) {
 
 document.addEventListener("submit", sayHello);
 
+// main one I was working with but doesn't seem to change alert / link when clicking the different buttons. I don't think the '= tru' is correct but was playing around with it!
+
 function pickColour(event) {
   event.preventDefault();
 
@@ -26,12 +28,25 @@ function pickColour(event) {
       "Good try but not quite there yet! Please check the colour wheel and pick again..."
     );
   } else if ((colourPickGreen = true)) {
-    alert(
-      "Good try but not quite there yet! Please check the colour wheel and pick again..."
-    );
+    alert("Good try but not quite there yet!");
   } else if ((colourPickOrange = true)) {
-    window.location.href = "https://www.google.co.uk/";
+    window.location = "https://www.google.co.uk/";
   }
 }
 
 document.addEventListener("click", pickColour);
+
+// suggested online?! (below)
+
+// document.getElementById("colour-buttons").addEventListener(
+//   "click",
+//   function (event) {
+//     var target = event.target;
+//     if (target.id == "purple-btn") {
+//       alert("Try again!");
+//     } else if (target.id == "green-btn") {
+//       alert("Nice try!");
+//     }
+//   },
+//   false
+// );
